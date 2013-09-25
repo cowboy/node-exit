129c86efa4b060ed69366b4317a31ab26e51c5be
Ben Alman <cowboy@rj3.net>
2013-09-25 09:25:48 -0400

    Rewriting unit tests to be MUCH more sane.
    * If you have to build tests dynamically, it's MUCH nicer to build actual TESTS dynamically, not just have a single test with a billion assertions in crazy async loops.
    * Seriously.

--- test/exit_test.js
+++ test/exit_test.js
@@ -22,7 +22,6 @@
 
 var fs = require('fs');
 var exec = require('child_process').exec;
-var async = require('async');
 
 var _which = require('which').sync;
 function which(command) {
@@ -37,28 +36,6 @@ function which(command) {
 // Look for grep first (any OS). If not found (but on Windows) look for find,
 // which is Windows' horribly crippled grep alternative.
 var grep = which('grep') || process.platform === 'win32' && which('find');
-
-function normalizeLineEndings(s) {
-  return s.replace(/\r?\n/g, '\n');
-}
-
-function run(command, options, callback) {
-  if (typeof options === 'function') {
-    callback = options;
-    options = {};
-  }
-  command += ' 2>&1';
-  if (options.pipe) {
-    command += ' | ' + grep + ' "std"';
-  }
-  exec(command, function(error, stdout) {
-    callback(command, error ? error.code : 0, normalizeLineEndings(stdout));
-  });
-}
-
-function fixture(filename) {
-  return normalizeLineEndings(String(fs.readFileSync(filename)));
-}
 
 exports['exit'] = {
   setUp: function(done) {
@@ -76,34 +53,69 @@ exports['exit'] = {
     test.ok(grep, 'A suitable "grep" or "find" program was not found in the PATH.');
     test.done();
   },
-  'stdout stderr': function(test) {
-    var counts = [10, 100, 1000];
-    var outputs = ['stdout stderr', 'stdout', 'stderr'];
-    test.expect(counts.length * outputs.length * 2);
-    async.eachSeries(counts, function(n, next) {
-      async.eachSeries(outputs, function(o, next) {
-        run('node log.js 0 ' + n + ' ' + o, {pipe: true}, function(command, code, actual) {
-          var expected = fixture(n + '-' + o.replace(' ', '-') + '.txt');
-          // Sometimes, the actual file lines are out of order on Windows.
-          // But since the point of this lib is to drain the buffer and not
-          // guarantee output order, we only test the length.
-          test.equal(actual.length, expected.length, '(length) ' + command);
-          // The "fail" lines in log.js should NOT be output!
-          test.ok(actual.indexOf('fail') === -1, '(no more output after exit) ' + command);
-          next();
-        });
-      }, next);
-    }, test.done);
-  },
-  'exit codes': function(test) {
-    var codes = [0, 1, 123];
-    test.expect(codes.length);
-    async.eachSeries(codes, function(n, next) {
-      run('node log.js ' + n + ' 10 stdout stderr', {pipe: false}, function(command, code) {
-        // The specified exit code should be passed through.
-        test.equal(code, n, 'should have exited with ' + n + ' error code.');
-        next();
-      });
-    }, test.done);
-  },
+  // The rest of the tests are built dynamically, to keep things sane.
 };
+
+// A few helper functions.
+function normalizeLineEndings(s) {
+  return s.replace(/\r?\n/g, '\n');
+}
+
+// Capture command output, normalizing captured stdout to unix file endings.
+function run(command, callback) {
+  exec(command, function(error, stdout) {
+    callback(error ? error.code : 0, normalizeLineEndings(stdout));
+  });
+}
+
+// Read a fixture file, normalizing file contents to unix file endings.
+function fixture(filename) {
+  return normalizeLineEndings(String(fs.readFileSync(filename)));
+}
+
+function buildTests() {
+  // Build individual unit tests for command output.
+  var counts = [10, 100, 1000];
+  var outputs = [' stdout stderr', ' stdout', ' stderr'];
+  var pipes = ['', ' | ' + grep + ' "std"'];
+  counts.forEach(function(count) {
+    outputs.forEach(function(output) {
+      pipes.forEach(function(pipe) {
+        var command = 'node log.js 0 ' + count + output + ' 2>&1' + pipe;
+        exports['exit']['output (' + command + ')'] = function(test) {
+          test.expect(2);
+          run(command, function(code, actual) {
+            var expected = fixture(count + output.replace(/ /g, '-') + '.txt');
+            // Sometimes, the actual file lines are out of order on Windows.
+            // But since the point of this lib is to drain the buffer and not
+            // guarantee output order, we only test the length.
+            test.equal(actual.length, expected.length, 'should be the same length.');
+            // The "fail" lines in log.js should NOT be output!
+            test.ok(actual.indexOf('fail') === -1, 'should not output after exit is called.');
+            test.done();
+          });
+        };
+      });
+    });
+  });
+
+  // Build individual unit tests for exit codes.
+  var codes = [0, 1, 123];
+  codes.forEach(function(code) {
+    var command = 'node log.js ' + code + ' 10 stdout stderr';
+    exports['exit']['exit code (' + command + ')'] = function(test) {
+      test.expect(1);
+      run(command, function(actual) {
+        // The specified exit code should be passed through.
+        test.equal(actual, code, 'should exit with ' + code + ' error code.');
+        test.done();
+      });
+    };
+  });
+}
+
+// Don't bother building tests if grep wasn't found, otherwise everything will
+// fail and the error will get lost.
+if (grep) {
+  buildTests();
+}
